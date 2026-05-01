f9c9cd4f382f2f4886480f44ca44da5b651b08dd
CheapNud <brechtvandeninden@hotmail.com>
2026-05-01 16:04:28 +0200

    Add pin/star priority to tasks

---
+++ Migrations/20260501140347_AddIsPinned.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CheapTasks.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIsPinned : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsPinned",
+                table: "Tasks",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tasks_OwnerId_IsPinned",
+                table: "Tasks",
+                columns: new[] { "OwnerId", "IsPinned" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Tasks_OwnerId_IsPinned",
+                table: "Tasks");
+
+            migrationBuilder.DropColumn(
+                name: "IsPinned",
+                table: "Tasks");
+        }
+    }
+}
